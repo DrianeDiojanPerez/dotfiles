ea15f7fd74867362eeb59522c7a6dad005f5e131
DrianeDiojanPerez <drianeperez12@gmail.com>
2025-09-27 23:58:54 -0600

    updated lsp config files and added color picker and intel driver

---
+++ nvim/.config/nvim/lsp/harper_ls.lua
@@ -0,0 +1,40 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "harper-ls", "--stdio" },
+	filetypes = {
+		"c",
+		"cpp",
+		"cs",
+		"gitcommit",
+		"go",
+		"html",
+		"java",
+		"javascript",
+		"lua",
+		"markdown",
+		"nix",
+		"python",
+		"ruby",
+		"rust",
+		"swift",
+		"toml",
+		"typescript",
+		"typescriptreact",
+		"haskell",
+		"cmake",
+		"typst",
+		"php",
+		"dart",
+		"clojure",
+		"sh",
+	},
+	root_markers = { ".git" },
+	settings = {
+		["harper-ls"] = {
+			linters = {
+				LongSentences = false,
+			},
+			userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+		},
+	},
+}
---
+++ nvim/.config/nvim/lsp/phpactor.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "phpactor", "language-server" },
+	filetypes = { "php" },
+	root_markers = { ".git", "composer.json", ".phpactor.json", ".phpactor.yml" },
+	workspace_required = true,
+	init_options = {
+		["language_server_phpstan.enabled"] = true,
+		["language_server_phpstan.level"] = "8",
+	},
+}
