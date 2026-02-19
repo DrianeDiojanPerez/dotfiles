751fb4e78fc35ce1891deddcffb25341c7b58c0c
DrianeDiojanPerez <drianeperez12@gmail.com>
2026-02-18 20:06:31 -0600

    updated the tmux-tree script and updated config for work tree binding

--- nvim/.config/nvim/lua/drianeperez/lazy/telescope.lua
+++ nvim/.config/nvim/lua/drianeperez/lazy/telescope.lua
@@ -36,7 +36,7 @@ return {
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>g", builtin.git_files, {})
+		-- vim.keymap.set("n", "<leader>g", builtin.git_files, {})
 
 		vim.keymap.set("n", "<leader>ws", function()
 			local word = vim.fn.expand("<cword>")
