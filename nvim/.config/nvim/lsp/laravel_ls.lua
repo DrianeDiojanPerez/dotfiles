55dabff6f898f16fa0fbd731a91bc15007b4b942
DrianeDiojanPerez <drianeperez12@gmail.com>
2026-05-01 22:26:45 -0600

    updated the code for laravel and php apps

--- nvim/.config/nvim/lsp/laravel_ls.lua
+++ nvim/.config/nvim/lsp/laravel_ls.lua
@@ -8,7 +8,16 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'laravel-ls' },
-  filetypes = { 'php', 'blade' },
-  root_markers = { 'artisan' },
+	cmd = { "laravel-ls" },
+	filetypes = { "php", "blade" },
+	root_markers = { "artisan" },
+    -- Added this because it was still attaching to non Laravel projects
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local root = vim.fs.root(fname, { "artisan" })
+		if root then
+			on_dir(root)
+		end
+		-- if no artisan found, don't call on_dir → server won't attach
+	end,
 }
